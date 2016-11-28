139d4ef2e06b69d22a10d55e29bfb9b43997c5a3
Mac <dada@Macintosh-2.local>
2016-11-28 10:23:10 +0800

    Initial Commit

---
+++ DDSnake/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDSnake
+//
+//  Created by DADA on 2016/11/28.
+//  Copyright © 2016年 DADA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DDSnake/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DDSnake
+//
+//  Created by DADA on 2016/11/28.
+//  Copyright © 2016年 DADA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
